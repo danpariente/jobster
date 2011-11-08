70e5734d94aa683db239a46796d277ea49732585
naughtystyle <dan_ownzzz@hotmail.com>
2011-11-08 12:02:00 -0400

    Update db/migrate/20111005202119_add_recipient_userid_to_messages.rb

--- db/migrate/20111005202119_add_recipient_userid_to_messages.rb
+++ db/migrate/20111005202119_add_recipient_userid_to_messages.rb
@@ -1,11 +1,9 @@
 class AddRecipientUseridToMessages < ActiveRecord::Migration
   def self.up
-    add_column :messages, :recipient_id, :integer
-    add_column :messages, :user_id, :integer
+    
   end
 
   def self.down
-    remove_column :messages, :user_id
-    remove_column :messages, :recipient_id
+    
   end
 end
